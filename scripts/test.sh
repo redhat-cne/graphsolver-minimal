bf8324b499d0ef2ea9ef8c542e137083d0cf6778
David Elie-Dit-Cosaque <deliedit@redhat.com>
2024-08-19 17:52:30 -0400

    Fix shfmt lint

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make build 
-./basic  2>&1| sed 's/time="[^"]*" //' > test/actual.txt
+make build
+./basic 2>&1 | sed 's/time="[^"]*" //' >test/actual.txt
 diff test/actual.txt test/expected.txt
